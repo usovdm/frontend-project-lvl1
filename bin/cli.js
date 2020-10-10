8c52dd0878ff0f3e37409746485b671ce371047b
Dmitry Usov <usovdm@gmail.com>
2020-10-10 22:51:27 +0300

    implements brain-even game

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,7 @@ const greeting = async () => {
   const question = 'May I have your name? '
   const name = await promptly.prompt(question)
   console.log(`Hello, ${name}!`)
+  return name
 }
 
 export default greeting
