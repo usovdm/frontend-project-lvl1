2a3d3f64d4989b30ed4e9ab9a872911f4bc463e4
Dmitry Usov <usovdm@gmail.com>
2020-10-12 17:06:59 +0300

    moves brain-calc to src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import brainEven from './games/even.js'
+import brainCalc from './games/calc.js'
 
 export default (game) => {
   if (game === 'even') {
     brainEven()
+  } else if (game === 'calc') {
+    brainCalc()
   }
 }
