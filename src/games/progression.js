71d5b7ed6ecb753ccb53bb079557d267a724c0be
Dmitry Usov <usovdm@gmail.com>
2020-10-19 12:53:07 +0300

    makes getProgressionWithHiddenNumber clear function

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,11 +1,17 @@
 import { getRandom } from '../utils.js'
 
-const getProgressionWithHiddenNumber = () => {
+const getRandomProgressionArguments = () => {
   const countOfItems = getRandom(5, 15)
   const hiddenIndex = getRandom(0, countOfItems - 1)
   const startNumber = getRandom(1, 20)
   const stepNumber = getRandom(1, 10)
 
+  return {
+    startNumber, countOfItems, stepNumber, hiddenIndex,
+  }
+}
+
+const getProgressionWithHiddenNumber = (startNumber, countOfItems, stepNumber, hiddenIndex) => {
   const progressionNumbers = []
   for (let i = 0; i <= countOfItems - 1; i += 1) {
     const number = startNumber + stepNumber * i
@@ -23,7 +29,13 @@ const getProgressionWithHiddenNumber = () => {
 }
 
 const getQuestion = () => {
-  const { progressionNumbers, hiddenNumber: correctAnswer } = getProgressionWithHiddenNumber()
+  const {
+    startNumber, countOfItems, stepNumber, hiddenIndex,
+  } = getRandomProgressionArguments()
+  const {
+    progressionNumbers,
+    hiddenNumber: correctAnswer,
+  } = getProgressionWithHiddenNumber(startNumber, countOfItems, stepNumber, hiddenIndex)
   const question = progressionNumbers.join(' ')
 
   return {
