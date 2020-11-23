4e1ee3744c0d580a64e48db07644721ae62b61de
Dmitry Usov <usovdm@gmail.com>
2020-11-23 14:59:04 +0300

    derefactors game engine

--- src/index.js
+++ src/index.js
@@ -2,32 +2,11 @@ import promptly from 'promptly';
 
 const WINS_LIMIT = 3;
 
-const greeting = async () => {
-  const question = 'May I have your name? ';
-  const name = await promptly.prompt(question);
-  console.log(`Hello, ${name}!`);
-  return name;
-};
+export default async (game) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = await promptly.prompt('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 
-const playRound = async (question, correctAnswer) => {
-  const answer = await promptly.prompt(`Question: ${question}`);
-  console.log(`Your answer: ${answer}!`);
-
-  const result = answer === correctAnswer;
-
-  if (result) {
-    console.log('Correct!');
-  } else {
-    console.log(
-      `'${answer}' is wrong answer ;(.`,
-      `Correct answer was '${correctAnswer}'.`,
-    );
-  }
-
-  return result;
-};
-
-const playGame = async (userName, game) => {
   const { instructions, getQuestion } = game;
 
   let winsCount = 0;
@@ -36,8 +15,16 @@ const playGame = async (userName, game) => {
   while (winsCount < WINS_LIMIT) {
     const { question, correctAnswer } = getQuestion();
     // eslint-disable-next-line no-await-in-loop
-    const roundIsWon = await playRound(question, correctAnswer);
-    if (!roundIsWon) {
+    const answer = await promptly.prompt(`Question: ${question}`);
+    console.log(`Your answer: ${answer}!`);
+
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `'${answer}' is wrong answer ;(.`,
+        `Correct answer was '${correctAnswer}'.`,
+      );
       console.log(`Let's try again, ${userName}!`);
       return;
     }
@@ -45,10 +32,4 @@ const playGame = async (userName, game) => {
   }
 
   console.log(`Congratulations, ${userName}!`);
-};
-
-export default async (game) => {
-  console.log('Welcome to the Brain Games!');
-  const userName = await greeting();
-  playGame(userName, game);
 };
